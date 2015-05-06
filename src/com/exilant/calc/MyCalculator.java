982e8ea3f0b6198c60ebaa4fdccef9b7177ed1ea
dmanasi <manasi.a.dharmadhikari@gmail.com>
2015-05-05 19:25:05 -0700

    simple calculator initial check in
    Simple Calculator Class with 4 public methods

---
+++ src/com/exilant/calc/MyCalculator.java
@@ -0,0 +1,26 @@
+package com.exilant.calc;
+
+public class MyCalculator 
+{
+	public int add(int num1, int num2)
+	{
+		return (num1 + num2);
+	}
+	
+	public int sub(int num1, int num2)
+	{
+		return (num1 - num2);
+	}
+
+	public int multiply(int num1, int num2)
+	{
+		return (num1 * num2);
+	}
+
+	public int divide(int num1, int num2)
+	{
+		return (num1 / num2);
+	}
+
+
+}
